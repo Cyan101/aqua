148acb537cb7e41668414bba40facf38d2f75a84
Jos Spencer <crazyandroid101@gmail.com>
2017-06-11 12:37:31 +0800

    Basic ping bot made

--- src/aqua.cr
+++ src/aqua.cr
@@ -1,5 +1,16 @@
 require "./aqua/*"
+require "discordcr"
 
 module Aqua
   # TODO Put your code here
+  bot_token = File.read("bot_token.txt")
+  client = Discord::Client.new(token: "Bot " + bot_token.strip, client_id: 323303029056995329_u64)
+
+  client.on_message_create do |payload|
+    if payload.content.starts_with? "!ping"
+      client.create_message(payload.channel_id, "Pong!")
+    end
+  end
+
+client.run
 end
