8aed54985206a3c2a1b0c4b2b3dba0d1f87ffacf
Jos Spencer <crazyandroid101@gmail.com>
2017-06-13 22:50:05 +0800

    Added uploading a file example

--- src/aqua/modules/sendimage.cr
+++ src/aqua/modules/sendimage.cr
@@ -8,6 +8,8 @@ module Aqua
       response = HTTP::Client.get "http://random.cat/meow"
       link = JSON.parse(response.body)["file"]
       CLIENT.create_message(payload.channel_id, link.as_s) # "#{link}" probably looks a bit nicer
+    when prefix + "kawaii", prefix + "cutie"
+      CLIENT.upload_file(payload.channel_id, "", File.open("src/aqua/images/cutie.png")) # "#{link}" probably looks a bit nicer
     end
   end
 end
