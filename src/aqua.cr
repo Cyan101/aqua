bfcdff238c621df3f94ad6c856d2da5592232a21
Jos Spencer <crazyandroid101@gmail.com>
2017-06-13 22:03:35 +0800

    Added Admin-y commands for the bot owner

--- src/aqua.cr
+++ src/aqua.cr
@@ -6,8 +6,24 @@ module Aqua
   CLIENT = Discord::Client.new(token: "Bot " + Config["token"].as_s, client_id: 323303029056995329_u64)
 
   CLIENT.on_message_create do |payload|
-    if payload.content.starts_with? "!ping"
-      CLIENT.create_message(payload.channel_id, "Pong!")
+    admin = true if payload.author.id == Config.["owner_id"].as_s.to_u64
+    if admin
+      content = payload.content.split
+      case content[0]
+      when "~cr"
+        content.shift
+        code = "crystal eval '#{content.join(" ")}'"
+        output = `#{code}`.chomp
+        CLIENT.create_message(payload.channel_id, output)
+      when "~rb"
+        content.shift
+        code = "ruby -e '#{content.join(" ")}'"
+        output = `#{code}`.chomp
+        CLIENT.create_message(payload.channel_id, output)
+      when "~say"
+        content.shift
+        CLIENT.create_message(payload.channel_id, content.join(" "))
+      end
     end
   end
 
