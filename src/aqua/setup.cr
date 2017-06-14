6b30d7082fe201751c729e9367865760555219db
Jos Spencer <crazyandroid101@gmail.com>
2017-06-14 17:56:12 +0800

    Added a config setup to replace the template

---
+++ src/aqua/setup.cr
@@ -0,0 +1,18 @@
+macro gets_scalar(value)
+  yaml.scalar "{{value}}"
+  puts "Enter {{value}}:"
+  yaml.scalar gets.not_nil!.chomp
+end
+
+unless File.exists?("config.yaml")
+  puts "No config found, creating Config.yaml"
+  yaml = YAML.build do |yaml|
+    yaml.mapping do
+      gets_scalar token
+      gets_scalar prefix
+      gets_scalar owner_id
+    end
+  end
+
+  File.write("config.yaml", yaml)
+end
