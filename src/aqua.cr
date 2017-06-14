e33cac4676fef8559e9599ddf81500c388d42ac0
Jos Spencer <crazyandroid101@gmail.com>
2017-06-14 18:09:19 +0800

    Fixed! Added a config setup to replace the template

--- src/aqua.cr
+++ src/aqua.cr
@@ -3,6 +3,7 @@ require "./aqua/requires.cr"
 Config = YAML.parse(File.read("config.yaml"))
 
 module Aqua
+
   CLIENT = Discord::Client.new(token: "Bot " + Config["token"].as_s, client_id: 323303029056995329_u64)
 
   CLIENT.on_message_create do |payload|
