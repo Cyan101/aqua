945ba1fe7926767651ff903039c58e2b71cde168
Jos Spencer <crazyandroid101@gmail.com>
2017-06-16 16:26:21 +0800

    Nicer way to setup a command list

--- src/aqua/modules/example.cr
+++ src/aqua/modules/example.cr
@@ -1,12 +1,16 @@
 module Aqua
-  prefix = Config["prefix"].as_s + ' '
-
   CLIENT.on_message_create do |payload|
-    command = payload.content
-    case command
-    when prefix + "help"
-      CLIENT.create_message(payload.channel_id, "I can't help you yet, sorry")
-    when prefix + "hi-five", prefix + "hifive", "o/"
+    content = payload.content.split
+    if content[0] == Config["prefix"].as_s
+      content.shift
+      case content[0]
+      when "help"
+        CLIENT.create_message(payload.channel_id, "I can't help you yet, sorry")
+      when "hug"
+        CLIENT.create_message(payload.channel_id, "*Hugs @<#{payload.author.id}>*")
+      end
+    end
+    if (content[0] == "hi-five") || (content[0] == "hifive") || (content[0] == "o/")
       next if payload.author.bot
       CLIENT.create_message(payload.channel_id, "o/")
     end
